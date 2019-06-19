3509c4f5eef3049654455b39d1af3f776b6cef2b
Mohammad Usman <mohammadusman666@gmail.com>
2019-06-19 19:07:51 +0500

    Added Login.py (Design)

---
+++ ui/loginUI.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.resize(400, 400)
+        MainWindow.setMinimumSize(QtCore.QSize(400, 400))
+        MainWindow.setMaximumSize(QtCore.QSize(400, 400))
+        MainWindow.setStyleSheet("* {\n"
+"    font-size: 15px;\n"
+"    font-family: Century Gothic, sans-serif;\n"
+"}\n"
+"\n"
+"QFrame {\n"
+"    background-image: url(:/resources/bg.jpg);\n"
+"}\n"
+"\n"
+"QLabel {\n"
+"    font-size: 40px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QLineEdit {\n"
+"    padding-left: 10px;\n"
+"    color: #405361;\n"
+"    border: 1px solid black;\n"
+"    border-radius: 5px;\n"
+"}\n"
+"\n"
+"QPushButton {\n"
+"    background-color: black;\n"
+"    color: rgb(200, 200, 200);\n"
+"    border-radius: 5px;\n"
+"}")
+        MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+        MainWindow.setTabShape(QtWidgets.QTabWidget.Rounded)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 400, 400))
+        self.frame.setStyleSheet("")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.lineEdit = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit.setGeometry(QtCore.QRect(50, 160, 300, 40))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit_2.setGeometry(QtCore.QRect(50, 230, 300, 40))
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setGeometry(QtCore.QRect(50, 300, 300, 40))
+        self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(50, 50, 300, 50))
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setIndent(0)
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Login"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Username"))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Password"))
+        self.pushButton.setText(_translate("MainWindow", "Login"))
+        self.label.setText(_translate("MainWindow", "The Quizzer"))
+
+# import bg_rc
